0e035d22cd2039138e78ff4f634546b0d4b25cb7
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2025-12-27 13:06:22 -0600

    DataTypes Usecases

--- 08-DataTypes.sh
+++ 08-DataTypes.sh
@@ -10,4 +10,4 @@ Sum=$(($Number1+$Number2+$Name)) # Arthimetic Expressions Always inside braces
 echo "Sum is :: ${Sum}"  # safe way to use variables
 
 Items=("Earth" "Water" "Fire" "Ash")
-echo "${Items}[@]"
+echo "${Items[*]}"
