9fd4fb853869272f0ed493c17a1e279a6b29d2b7
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2025-12-27 11:49:08 -0600

    Special Variables usecase

--- 07-SpecialVar.sh
+++ 07-SpecialVar.sh
@@ -3,4 +3,9 @@
 echo "All Variables Passed to the Script are :: $@"
 echo "All Variables Passed to the Script are :: $*"
 echo "Current Running Script :: $0"
+echo "$PWD"
+echo "$USER"
+echo "$HOME"
+echo "PID of Current Script :: $$"
+echo "PID of last CMD :: $!"
 
