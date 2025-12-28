5d7bcd564734ff8b4c4d6b9960bdee31ed182c30
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2025-12-27 18:34:28 -0600

    Installing SQL Script

--- 11-Installation.sh
+++ 11-Installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR :: Please Get Admin Access"
+    exit 1 # Other than 0 means failure
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR :: Installing MySQL is Failed"
+    exit 1
 else
     echo "Installing MySQL is SUCCESS"
 fi        
