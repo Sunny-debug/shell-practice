bd18f8e59651cb574a7a51a8ce2b5dfa93c8136c
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2025-12-30 17:11:54 -0600

    Practice

---
+++ 19-script1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+NAME=Sunny
+echo "My name is :$NAME"
+echo "PID of current script-1 :: $$"
+source ./20-script2.sh    # . Represents Current dir
---
+++ 20-script2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "My name is :: $NAME"
+echo "PID of Script2 - $$"
+
