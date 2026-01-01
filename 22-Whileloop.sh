e2602ede750a90f86cac22272c95102a8bc06c71
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2026-01-01 14:43:16 -0600

    Practice

--- 22-Whileloop.sh
+++ 22-Whileloop.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-COUNT=5
+# COUNT=5
 
-while [ $COUNT -gt 0 ]; do
-    echo "Time Left: $COUNT"
-    sleep 1
-    COUNT=$((COUNT - 1))    
-done
+# while [ $COUNT -gt 0 ]; do
+#     echo "Time Left: $COUNT"
+#     sleep 1
+#     COUNT=$((COUNT - 1))    
+# done
 
-echo "Time's up!!!!"
+# echo "Time's up!!!!"
+
+#!/bin/bash
+
+# Assign the file name to a variable
+
+# Check if the file exists
+    # Use a while loop with read to process the file line by line
+while IFS= read -r line; do
+        # Process the line stored in the variable $line
+    echo "Line read: $line"
+        # Add your custom commands here
+done < 19-script1.sh
