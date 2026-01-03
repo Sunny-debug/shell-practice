fa1006a134a06948d232328ff0f45ad0a8aa1d8a
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2026-01-02 19:13:04 -0600

    Practice

--- 25-Disk-usage.sh
+++ 25-Disk-usage.sh
@@ -2,15 +2,15 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2 
-IP_ADDRESS=$(curl curl http://169.254.169.254/latest/meta-data/local-ipv4)
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 MESSAGE=""
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk '{print $6}' | cut -d '%' -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        MESSAGE+="High Disk Usage on $PARTITION: $USAGE %"
+        MESSAGE+="High Disk Usage on $PARTITION: $USAGE %\n"
     fi
 done <<< $DISK_USAGE
 
-echo "Message Body: $MESSAGE\n"
+echo "Message Body: $MESSAGE"
