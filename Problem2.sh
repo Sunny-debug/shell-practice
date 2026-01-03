77a17703302797bc88db60e7b69ccf2e0c8dfa53
Bhargav Vaddepally <bhargav.vaddepally@siu.edu>
2026-01-03 13:28:25 -0600

    Practice

--- Problem2.sh
+++ Problem2.sh
@@ -2,8 +2,24 @@
 
 FILENAME=$1
 
+count(){
+    wc -w $FILENAME
+    wc -l $FILENAME
+    wc -c $FILENAME
+}
+
 if [ $# -eq 0 ]; then
     echo "Please Enter the Filename"
-
-lines=$(wc -l $FILENAME)
-echo "No of lines in the $FILENAME:: $lines"
+    exit 1
+elif [ ! -e "$FILENAME" ]; then
+    echo "File doesnot exist"
+    exit 1
+elif [[ ! -r "$FILENAME" ]]; then
+    echo "File is not readable"    
+    exit 1
+elif [ ! -s "$FILENAME" ]; then
+    echo "Count=0"
+    exit 0
+else
+    count
+fi   
